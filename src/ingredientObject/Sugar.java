5bb4ce61c695ad752c5ef42d11d9a0634969c5f8
m-blaz <blaz@mail.ru>
2020-09-20 13:58:59 +0300

    Добавил ингредиент сахар

---
+++ src/ingredientObject/Sugar.java
@@ -0,0 +1,9 @@
+package ingredientObject;
+
+import tasteObject.Taste;
+
+public class Sugar extends DefaultIngredient {
+    public Sugar(String name, Taste taste, int weight) {
+        super(name, taste, weight);
+    }
+}
